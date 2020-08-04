d07089e8302372df5ff12131d410f26ed0c369fc
Khalil <khalilfoulks@gmail.com>
2020-08-04 18:51:13 -0400

    finished delete and put endpoints

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -34,7 +34,6 @@ router.get("/:id", (req, res) => {
 })
 
 router.get("/:id/comments", (req, res) => {
-    const text = req.body
     const id = req.params.id
 
     db.findById(id)
@@ -109,6 +108,48 @@ router.post("/:id/comments", (req, res) => {
         res.status(500).json({ error: "The posts information could not be retrieved." })
     })  
 })
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id
+    
+    db.remove(id)
+    .then(item => {
+        if(item) {
+            console.log("post deleted")
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    }) 
+})
+
+router.put("/:id", (req,res) => {
+    const id = req.params.id
+    const post = req.body
+
+    if(!post.title || !post.contents){
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else{
+        db.update(id, post)
+        .then(item => {
+            if(item > 0) {
+                res.status(200).json({ post })
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The posts information could not be retrieved." })
+        }) 
+    }
+})
+
+
 
 module.exports = router;
 
