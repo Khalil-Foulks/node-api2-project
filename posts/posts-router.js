55e19a38d117035ab02f0cbce614736d75be418c
Khalil <khalilfoulks@gmail.com>
2020-08-04 16:29:28 -0400

    finished POST endpoint

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -15,5 +15,37 @@ router.get("/", (req, res) => {
         })
 })
 
+router.get("/:id", (req, res) => {
+    db.findById(req.params.id)
+        .then( database =>{
+            if(database){
+                res.status(200).json({ database })
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The posts information could not be retrieved." })
+        })
+})
+
+router.post("/", (req, res) => {
+    const post = req.body
+    
+    if(!post.title || !post.contents){
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        db.insert(post)
+        .then(database => {
+            res.status(201).json({ post })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+    }
+})
+
 module.exports = router;
 
